318668070998b67a1c897ec39f63700b053d11b7
ivanilson.zapelini <ivanilsonz@gmail.com>
2018-07-16 17:51:40 -0300

    Add default host 0.0.0.0.

--- src/pyrrot.py
+++ src/pyrrot.py
@@ -5,6 +5,7 @@ from _pyrrot.main import create_app
 
 def _create_args():
     parser = argparse.ArgumentParser(description='Foo')
+    parser.add_argument('-h', '--host', help='The host of the pyrrot. Defaults to 0.0.0.0', type=str, default='0.0.0.0')
     parser.add_argument('-p', '--port', help='The port of the pyrrot. Defaults to 1234', type=int, default=1234)
     requiredNamed = parser.add_argument_group('required named arguments')
     requiredNamed.add_argument('-c', '--conf', help='Directory path to read configurations', required=True)
@@ -14,12 +15,14 @@ def _create_args():
 def main(developer_mode=False):
     conf = '../examples'
     port = 1234
+    host = '0.0.0.0'
     if not developer_mode:
         args = _create_args()
         conf = args.conf
         port = args.port
+        host = args.host
     app = create_app(debug=True, configuration=conf)
-    app.run(port=port)
+    app.run(host=host, port=port)
 
 
 if __name__ == "__main__":
